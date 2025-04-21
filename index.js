00ca7461c7b7fc8fad69197a0707c5c80bbffe6d
Abadur Rahman <abadur391@gmail.com>
2025-04-21 16:06:30 +0600

    Migrated to OpenAI

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-import { GoogleGenAI } from "@google/genai";
 import "dotenv/config"
 
-const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+import OpenAI from "openai";
 
-async function main() {
-  const response = await ai.models.generateContent({
+const openai = new OpenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+});
+
+const response = await openai.chat.completions.create({
     model: "gemini-2.0-flash",
-    contents: "Explain how AI works in a few words",
-  });
-  console.log(response.text);
-}
+    messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        {
+            role: "user",
+            content: "Explain to me how AI works",
+        },
+    ],
+});
 
-main();
+console.log(response.choices[0].message);
